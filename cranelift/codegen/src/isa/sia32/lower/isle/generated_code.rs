2b601757a97f7d37542393a407ee971fce0204c5
Nick Zbinden <niklaus.zbinden@gmail.com>
2026-09-16 22:21:55 +0200

    M5: add generated SIA32 ISLE include

---
+++ cranelift/codegen/src/isa/sia32/lower/isle/generated_code.rs
@@ -0,0 +1,11 @@
+// Generated at build time from sia32/{inst,lower}.isle.
+#![expect(
+    dead_code,
+    unreachable_patterns,
+    unused_imports,
+    unused_variables,
+    irrefutable_let_patterns,
+    clippy::clone_on_copy,
+    reason = "generated ISLE code"
+)]
+include!(concat!(env!("ISLE_DIR"), "/isle_sia32.rs"));
