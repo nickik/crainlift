9590627d92c3bb9ee1e678f7193b30d59954c432
Nick Zbinden <niklaus.zbinden@gmail.com>
2026-09-16 22:00:14 +0200

    M5: add SIA32 LowerBackend entry points

---
+++ cranelift/codegen/src/isa/sia32/lower.rs
@@ -0,0 +1,22 @@
+//! SIA32 CLIF lowering entry points.
+
+use crate::ir::Inst as IRInst;
+use crate::isa::sia32::Sia32Backend;
+use crate::isa::sia32::inst::Inst;
+use crate::machinst::*;
+
+pub mod isle;
+
+impl LowerBackend for Sia32Backend {
+    type MInst = Inst;
+
+    fn lower(&self, ctx: &mut Lower<Inst>, ir_inst: IRInst) -> Option<InstOutput> {
+        isle::lower(ctx, self, ir_inst)
+    }
+
+    fn lower_branch(&self, ctx: &mut Lower<Inst>, ir_inst: IRInst, targets: &[MachLabel]) -> Option<()> {
+        isle::lower_branch(ctx, self, ir_inst, targets)
+    }
+
+    fn maybe_pinned_reg(&self) -> Option<Reg> { None }
+}
