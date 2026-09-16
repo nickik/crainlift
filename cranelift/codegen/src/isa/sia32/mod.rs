394a5792c52b49b111cd29551d7c4a56fae29e71
Nick Zbinden <niklaus.zbinden@gmail.com>
2026-09-16 08:24:49 +0200

    Use direct SIA32 modules with ABI helper bridge

--- cranelift/codegen/src/isa/sia32/mod.rs
+++ cranelift/codegen/src/isa/sia32/mod.rs
@@ -21,11 +21,10 @@ use core::fmt;
 use cranelift_control::ControlPlane;
 use target_lexicon::{Architecture, Triple};
 
-#[path = "abi_module.rs"]
 mod abi;
 mod abi_contract;
-#[path = "inst_module.rs"]
 mod inst;
+mod inst_abi_compat;
 mod label;
 #[allow(missing_docs)]
 pub mod encode;
