d50c068a1b2c465031d2c3057757c8752919051c
Nick Zbinden <niklaus.zbinden@gmail.com>
2026-09-20 15:38:05 +0200

    SIA32: model GPR boundary operations as explicit moves

--- cranelift/codegen/src/isa/sia32/inst.rs
+++ cranelift/codegen/src/isa/sia32/inst.rs
@@ -360,8 +360,8 @@ impl MachInst for Inst {
             Self::Nop | Self::SoftwareTrap { .. } | Self::Fence | Self::SRet | Self::TlbFence
             | Self::Wfi | Self::SyncI | Self::Jump { .. } | Self::Ret => {}
             Self::SRead { dst, .. } => collector.reg_def(dst),
-            Self::ReadFixedGpr { dst, index } => collector.reg_fixed_def(dst, regs::preg(*index).into()),
-            Self::WriteFixedGpr { src, index } => collector.reg_fixed_use(src, regs::preg(*index).into()),
+            Self::ReadFixedGpr { dst, .. } => collector.reg_def(dst),
+            Self::WriteFixedGpr { src, .. } => collector.reg_use(src),
             Self::SWrite { src, .. } | Self::SRetCtx { src } | Self::TlbFenceVa { src }
             | Self::TlbFenceAsid { src } => collector.reg_use(src),
             Self::SSwapScratch { dst, src } => { collector.reg_use(src); collector.reg_def(dst); }
