d46a3545eac07fe99a08b6d39270274dcbc8ab2b
Nick Zbinden <niklaus.zbinden@gmail.com>
2026-09-21 02:19:13 +0200

    Gate ISLE unwind re-export with unwind feature

--- cranelift/codegen/src/machinst/isle.rs
+++ cranelift/codegen/src/machinst/isle.rs
@@ -6,7 +6,9 @@ use smallvec::SmallVec;
 pub use super::MachLabel;
 use super::RetPair;
 pub use crate::ir::{condcodes::CondCode, *};
-pub use crate::isa::{TargetIsa, unwind::UnwindInst};
+pub use crate::isa::TargetIsa;
+#[cfg(feature = "unwind")]
+pub use crate::isa::unwind::UnwindInst;
 pub use crate::machinst::{
     ABIArg, ABIArgSlot, ABIMachineSpec, InputSourceInst, Lower, LowerBackend, RealReg, Reg,
     RelocDistance, Sig, TryCallInfo, VCodeInst, Writable,
