c08f71e0b8c72cd219af14aa358a8ae27a551cb9
Nick Zbinden <niklaus.zbinden@gmail.com>
2026-09-21 04:42:03 +0200

    cranelift: allow legacy Rust 2018 lifetime lints

--- cranelift/bforest/src/lib.rs
+++ cranelift/bforest/src/lib.rs
@@ -13,7 +13,7 @@
 //! - Empty trees have a very small 32-bit footprint.
 //! - All the trees in a forest can be cleared in constant time.
 
-#![deny(missing_docs)]
+#![allow(elided_lifetimes_in_paths, explicit_outlives_requirements)]\n#![deny(missing_docs)]
 #![no_std]
 
 #[cfg(test)]
